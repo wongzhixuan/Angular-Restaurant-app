b2d4227148cd779e3e0cb90fa4c77f5bbb6163c6
wongzhixuan <annie13075@gmail.com>
2022-08-18 17:07:52 +0800

    4 files modified, 10 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,16 @@ import {DishdetailComponent }from './dishdetail/dishdetail.component';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {DishService} from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
