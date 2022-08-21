760e47f34109b55bb7b4d2ffbc1d10a152aef5cd
Wong Zhi Xuan <annie13075@gmail.com>
2022-08-21 16:53:27 +0800

    Template-driven forms P1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,28 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { DishService } from './services/dish.service';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+
+// router links
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+
 import { MatButtonModule } from '@angular/material/button';
 import { PromotionService } from './services/promotion.service';
+
+// dialog
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
+
+// angular forms
+import { MatFormFieldModule } from '@angular/material/form-field';  // group form elements
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; // template driven forms
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,9 +56,13 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
-  entryComponents:[
+  entryComponents: [
     LoginComponent,
   ],
   providers: [DishService, PromotionService],
